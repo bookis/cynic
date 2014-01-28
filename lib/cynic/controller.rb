e2bbee85a1341110b0f31e7050331b1c02f13dfa
Bookis Smuin <bookis.smuin@gmail.com>
2014-01-27 20:29:05 -0800

    adds controller docs

--- lib/cynic/controller.rb
+++ lib/cynic/controller.rb
@@ -5,12 +5,19 @@ module Cynic
       Renderer.new(full_path).body
     end
 
+    # The +name+ split and joined into a string seperated by "/"'s
     def path
-      self.class.to_s.split("::").join("/").downcase.gsub(/controller$/i, "")
+      name.split("::").join("/").downcase
     end
     
+    # The +path+ plus the current action.html.erb
     def full_path
       path + "/#{@action}.html.erb"
+    end
+    
+    # The Controllers class name without the word `controller` in it.
+    def name
+      self.class.to_s.gsub(/controller$/i, "")
     end
   end
 end
